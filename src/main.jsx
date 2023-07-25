fdd6b44edbb269f9082d63eaf08a0523475adb46
Kris Stern <krisstern@outlook.com>
2023-07-25 20:03:35 +0800

    Complete draft

--- src/main.jsx
+++ src/main.jsx
@@ -52,40 +52,8 @@ const router = createBrowserRouter([
     }]
   },
 ], {
-  basename: "/contacts"
+  basename: "/contact"
 });
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route
-//       path="/"
-//       element={<Root />}
-//       loader={rootLoader}
-//       action={rootAction}
-//       errorElement={<ErrorPage />}
-//     >
-//       <Route errorElement={<ErrorPage />}>
-//         {/*<Route index element={<Index />} />*/}
-//         <Route
-//           path="contacts/:contactId"
-//           element={<Contact />}
-//           loader={contactLoader}
-//           // action={contactAction}
-//         />
-//         <Route
-//           path="contacts/:contactId/edit"
-//           element={<EditContact />}
-//           loader={contactLoader}
-//           action={editAction}
-//         />
-//         <Route
-//           path="contacts/:contactId/destroy"
-//           // action={destroyAction}
-//         />
-//       </Route>
-//     </Route>
-//   )
-// );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
