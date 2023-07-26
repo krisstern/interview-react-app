c6b517f98d33e0e33d99821f8a2387d370d54602
Kris Stern <krisstern@outlook.com>
2023-07-26 16:00:37 +0800

    Add return to home link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,17 @@
-import '../Nav.css'
+import '../Sidebar.css'
+import {NavLink} from "react-router-dom";
 
 function Sidebar() {
 
   return (
     <>
       <div id="sidebar2">
-        <h1><strong>Rick and Morty</strong></h1>
+        <NavLink
+          to={"/"}
+          style={{ textDecoration: 'none' }}
+        >
+          <h1><strong>Rick and Morty</strong></h1>
+        </NavLink>
         <h2>Contacts</h2>
       </div>
     </>
